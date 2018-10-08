76c39ca19e82884ed3ef6549cc5c771586682bc1
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-10-08 19:42:10 +0000

    automatically backed up by learn

--- dogs/lib/dogs/cli.rb
+++ dogs/lib/dogs/cli.rb
@@ -37,7 +37,6 @@
     puts "To go back to our adoptions menu type list if you had enough cuteness type exit"     
 
   elsif input == "list"
-    puts "Whoops!! invalid selection please type list or exit \u{1f60e}"
     welcome_method
   
   else
