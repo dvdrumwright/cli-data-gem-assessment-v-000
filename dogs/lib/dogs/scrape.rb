2d4f2904ee0a25b0a82c324a4d2c8b8b4fb4707a
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-10-12 02:38:07 +0000

    Done.

--- dogs/lib/dogs/scrape.rb
+++ dogs/lib/dogs/scrape.rb
@@ -1,19 +1,19 @@
     class Scrape
-
     
-    def self.make_dogs
-        url = "https://waggingtailsrescue.org/"
-        doc = Nokogiri::HTML(open(url))
         
-        doc_list  = doc.css("a.project")
-        doc_list.each.with_index do |element, i| 
-      
+    def self.make_dogs
+      url = "https://waggingtailsrescue.org/"
+      doc = Nokogiri::HTML(open(url))
+            
+      doc_list  = doc.css("a.project")
+      doc_list.each.with_index do |element, i| 
+          
     next if doc_list.css("div.project-title")[i].text == "Weekly Adoptions"
-        dog = Dog.new 
-        dog.name = doc_list.css("div.project-title")[i].text 
-        dog.info = doc.css("div.project-meta")[i].text
-        dog.save 
-       
+      dog = Dog.new 
+      dog.name = doc_list.css("div.project-title")[i].text 
+      dog.info = doc.css("div.project-meta")[i].text
+      dog.save 
+           
         end 
       end 
     end 
