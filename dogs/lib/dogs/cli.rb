2bea7ecb9890ed049d088aeb1af5a2cfa8dd2048
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-10-12 01:28:08 +0000

    Done.

--- dogs/lib/dogs/cli.rb
+++ dogs/lib/dogs/cli.rb
@@ -9,43 +9,44 @@
 
   
    
-  def welcome_method 
-    puts "\u{1f436}  Welcome to Wagging Tails Rescue  \u{1f436}"
-    @scraping = Dog.all 
-    @scraping.each.with_index(1) do |recipe, i| 
-    puts "#{i}. #{recipe.name}" 
+    def welcome_method 
+       puts "\u{1f436}  Welcome to Wagging Tails Rescue  \u{1f436}"
+       @scraping = Dog.all 
+       @scraping.each.with_index(1) do |recipe, i| 
+       puts "#{i}. #{recipe.name}" 
     end 
-   puts "Please choose from the following selections \u{1f436}"
+       puts "Please choose from the following selections \u{1f436}"
   end 
 
   
   def input_from_user_method 
-    input = nil
+      input = nil
    
   while input != "exit" 
-    input = gets.strip.downcase 
-    inp = input.to_i
+       input = gets.strip.downcase 
+       inp = input.to_i
     
     
   if inp.between?(1,@scraping.size)
-    the_scraper = @scraping[inp - 1]
-    puts "#{the_scraper.name}"  
-    puts "Great choice if you would like more information regarding your option type INFO"
-  
-  elsif input == "info"
-    puts "#{the_scraper.info}"
-    puts "To see our selections again please type LIST or EXIT"      
-  
-  elsif input == "list"
-    welcome_method
-    elsif input == "exit"
-  break 
-       
-  else 
-      puts "\u{1f640} Whoops invalid choice to go back to our main menu type List"
+        the_scraper = @scraping[inp - 1]
+        puts "#{the_scraper.name}"  
+        puts "Great choice if you would like more information regarding your option type INFO"
       
-  end 
-  end 
+  elsif input == "info"
+        puts "#{the_scraper.info}"
+        puts "To see our selections again please type LIST or EXIT"      
+      
+  elsif input == "list"
+        welcome_method
+  
+  elsif input == "exit"
+  
+  break 
+    else 
+        puts "\u{1f640} Whoops invalid choice to go back to our main menu type List"
+      
+      end 
+    end 
   end 
  
  
